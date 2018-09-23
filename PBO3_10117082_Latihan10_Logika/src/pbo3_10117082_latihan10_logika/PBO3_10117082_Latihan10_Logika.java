fc00d7d47e86a9ba2938de18d96031f51da926ab
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:22:38 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan10_Logika/src/pbo3_10117082_latihan10_logika/PBO3_10117082_Latihan10_Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan10_logika;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM    : 10117082
+ */
+public class PBO3_10117082_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+        
+        
+        
+        
+    }
+    
+}
